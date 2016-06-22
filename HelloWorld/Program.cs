4114096a3e5a7bc6f989c4f8b31210e703a8cc28
Richard Wrench <rwrench@gmail.com>
2016-06-21 20:24:08 -0500

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Exchange.WebServices.Data;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2013_SP1);
+           service.Credentials = new WebCredentials("SP", ")(*0o9i8u","dpsol");
+           service.AutodiscoverUrl("salespoint@dpsol.com", RedirectionUrlValidationCallback);
+
+            EmailMessage email = new EmailMessage(service);
+            email.ToRecipients.Add("rwrench@dpsol.com");
+            email.Subject = "HelloWorld";
+            email.Body = new MessageBody("This is the first email I've sent by using the EWS Managed API");
+            email.Send();
+        }
+
+        private static bool RedirectionUrlValidationCallback(string redirectionUrl)
+        {
+            // The default for the validation callback is to reject the URL.
+            bool result = false;
+
+            Uri redirectionUri = new Uri(redirectionUrl);
+            
+
+            // Validate the contents of the redirection URL. In this simple validation
+            // callback, the redirection URL is considered valid if it is using HTTPS
+            // to encrypt the authentication credentials. 
+            if (redirectionUri.Scheme == "https")
+            {
+                result = true;
+            }
+            return result;
+        }
+    }
+}
